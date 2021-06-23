1c69602a38bc896969472ce5d5c04b95379e64d2
Mjkli <Mjkli@users.noreply.github.com>
2021-06-22 19:51:02 -0700

    Adding Navbar

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Management',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Research and Development',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Security',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Demo',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Investors?',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
