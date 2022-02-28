537fd3e09b73f2c2a9f137d5f274acf41a33598f
Anthony Bruno <anthony.bruno@automotivepower.com>
2022-02-28 10:13:22 -0800

    Small Test

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -10,7 +10,7 @@ function Herosection(){
             <h1>Look Here!</h1>
             <div className="hero-btns">
                 <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>START</Button>
-                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Watch TRALIER <i className ="far fa-play-circle"/></Button>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Watch TRALIERS <i className ="far fa-play-circle"/></Button>
             </div>
 
         </div>
