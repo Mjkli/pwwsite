93b6c957cc3ee297ecd03e7f325632ffa86230c3
Anthony <anthonybruno63@yahoo.com>
2026-08-01 13:29:19 -0700

    footer dont now

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -8,34 +8,34 @@ function NavBar({ setMainPage }: { setMainPage: (page: string) => void }) {
         <>  
             { isNavHidden ? null : (
                 <div className="navbar">
-                    <button onClick={() => setMainPage("Home")}>
+                    <button className="navbutton" onClick={() => setMainPage("Home")}>
                         Home
                     </button>
-                    <button type="button" onClick={() => setMainPage("Management")}>
+                    <button className="navbutton" type="button" onClick={() => setMainPage("Management")}>
                         Management
                     </button>
-                    <button type="button" onClick={() => setMainPage("Research and Development")}>
+                    <button className="navbutton" type="button" onClick={() => setMainPage("Research and Development")}>
                         Research and Development
                     </button>
-                    <button type="button" onClick={() => setMainPage("Security")}>
+                    <button className="navbutton" type="button" onClick={() => setMainPage("Security")}>
                         Security
                     </button>
-                    <button type="button" onClick={() => setMainPage("Demo")}>
+                    <button className="navbutton" type="button" onClick={() => setMainPage("Demo")}>
                         Demo
                     </button>
-                    <button type="button" onClick={() => setMainPage("Investors")}>
+                    <button className="navbutton" type="button" onClick={() => setMainPage("Investors")}>
                         Investors
                     </button>
-                    <button type="button" onClick={() => setMainPage("Merch")}>
+                    <button className="navbutton" type="button" onClick={() => setMainPage("Merch")}>
                         Merch
                     </button>
-                    <button type="button" onClick={() => setMainPage("Contact Us")}>
+                    <button className="navbutton" type="button" onClick={() => setMainPage("Contact Us")}>
                         Contact Us
                     </button>
                 </div>
             )}
             <button className="hamburger" onClick={() => setIsNavHidden(!isNavHidden)}>
-                ☰
+                {isNavHidden ? <i className="fas fa-bars"></i> : <i className="fas fa-times"></i>}
             </button>
         </>
     );
