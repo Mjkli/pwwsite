ddfe403c5c114ffd81c265c178c1f0c3d6c7f679
Anthony <anthonybruno63@yahoo.com>
2026-08-15 16:34:58 -0700

    adding cloud front

---
+++ tf-infra/cloudFront.tf
@@ -0,0 +1,48 @@
+resource "aws_cloudfront_origin_access_control" "pww_oac" {
+  name                              = "pww-oac"
+  description                       = "CloudFront access to S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "pww_cdn" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.pww_bucket.bucket_regional_domain_name
+    origin_id                = "pww-s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.pww_oac.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "pww-s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.pww_cdn.domain_name
+}
--- tf-infra/s3.tf
+++ tf-infra/s3.tf
@@ -1,3 +1,42 @@
 resource "aws_s3_bucket" "pww-bucket" {
   bucket = "pww-mjkli-bucket"
 }
+
+resource "aws_s3_bucket_public_access_block" "pww-pab" {
+  bucket = aws_s3_bucket.pww-bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "pww_bucket_policy" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.pww_bucket.arn}/*"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+
+      values = [
+        aws_cloudfront_distribution.pww_cdn.arn
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "pww_bucket_policy" {
+  bucket = aws_s3_bucket.pww_bucket.id
+  policy = data.aws_iam_policy_document.pww_bucket_policy.json
+}
