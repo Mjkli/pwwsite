1859bc537b6ba349dd0492d5eebece15b4071a66
anthony <anthonybruno63@yahoo.com>
2026-08-14 19:55:01 -0700

    adding home pictures

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,12 @@
 import '../App.css'
 import Header from '../components/Header/Header'
-import PageTabs from '../components/PageTabs/PageTabs'
 
 const Home = ({ setMainPage }: { setMainPage: (page: string) => void }) => {
 
     return(
         <>
             <Header setMainPage={setMainPage} />
-            <div className="page-content">
-                <h1>Home</h1>
-            </div>
+            <div className="photo-container invest-photo"></div>
         </>
     )
 }
