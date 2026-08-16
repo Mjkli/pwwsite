77d5065ba85a1319f3f4e7050c3c580ed68acba2
Anthony <anthonybruno63@yahoo.com>
2026-08-15 20:29:50 -0700

    updating for mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import '../App.css';
 import '../components/Core/core.css';
 import '../components/Footer/footer.css'
+import '../components/Core/core.css'
 import Header from '../components/Header/Header';
 import SlideShow from '../components/SlideShow/slide';
 
@@ -10,7 +11,9 @@ const Home = ({ setMainPage }: { setMainPage: (page: string) => void }) => {
     return(
         <>
             <Header setMainPage={setMainPage} />
-            <SlideShow />
+            <div className="home-base">
+                <SlideShow />
+            </div>
             <div className="footer-bottom footer-fixed">
                 <p>© 2024 PWW. All rights reserved.</p>
             </div>
