f11ca809158307227a4fed3f0a3619b7bb93bee9
Anthony <anthonybruno63@yahoo.com>
2026-08-15 18:08:41 -0700

    Attaching cert to website

--- tf-infra/cloudFront.tf
+++ tf-infra/cloudFront.tf
@@ -44,7 +44,10 @@ resource "aws_cloudfront_distribution" "pww_cdn" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = aws_acm_certificate_validation.pww.certificate_arn
+
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
 
